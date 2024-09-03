3e381c7c9e077a5627b3294f0c34bcd8a8a4075d
jonatan.compiani <jonatan.compiani@softdesign.com.br>
2024-09-02 21:38:19 -0300

    Add project files.

---
+++ Kafka.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
+using System.Threading;
+
+Console.WriteLine("Starting Consumer");
+
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "Group01",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using(var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe(["messages"]);
+
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine($"[{consumeResult.Offset}] {consumeResult.Message.Value}");
+        Thread.Sleep(1000);
+    }
+
+    consumer.Close();
+}
---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+Console.WriteLine("Starting Producer");
+
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+string inputText;
+do
+{
+    inputText = Console.ReadLine();
+    if (!string.IsNullOrEmpty(inputText))
+    {
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            string topicName = "messages";
+
+            //producer.Produce(topicName, new() { Value = "my message"});
+
+            var result = await producer.ProduceAsync(topicName, new() { Value = inputText });
+
+            Console.WriteLine($"Added on Offset {result.Offset}");
+        }
+    }
+} while (!string.IsNullOrEmpty(inputText));
+
+
+
